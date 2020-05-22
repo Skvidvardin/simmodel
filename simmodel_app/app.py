4318e85c4bb1906f0517c11538ac896bebe09332
skvid <max_pershin@bk.ru>
2020-05-22 16:58:40 +0300

    final model

--- simmodel_app/app.py
+++ simmodel_app/app.py
@@ -15,8 +15,8 @@ import pandas as pd
 
 import plotly.graph_objects as go
 
-# import qdc
-from simmodel_app import qdc  # for DEBUG
+import qdc
+# from simmodel_app import qdc  # for DEBUG
 
 FONT_AWESOME = "./assets/fontawesome-free-5.13.0-web/css/all.css"
 
